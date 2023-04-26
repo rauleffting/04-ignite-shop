ff5e0da0c9c0f29e96640e9523a2fcfe9feb2f09
raul <rauleffting94@gmail.com>
2023-04-26 20:24:35 -0300

    add stitches

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,3 +1,20 @@
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+import { AppProps } from 'next/app'
+import { globalStyles } from '../styles/global'
+
+import logoImg from '../assets/logo.svg'
+import { Container, Header } from '../styles/pages/app'
+
+/* the best is to put globalStyles outside the function App because if it's inside, it will be rendered every time the function App is rendered */
+globalStyles()
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <Container>
+      <Header>
+        <img src={logoImg.src} alt="" />
+      </Header>
+
+      <Component {...pageProps} />
+    </Container>
+  )
 }
