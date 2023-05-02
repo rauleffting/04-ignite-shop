cf57fa19caccd8b04084368322aeb5bef7ef4777
raul <rauleffting94@gmail.com>
2023-05-02 18:32:08 -0300

    add layout and carousel

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,6 +4,8 @@ import { globalStyles } from '../styles/global'
 import logoImg from '../assets/logo.svg'
 import { Container, Header } from '../styles/pages/app'
 
+import Image from 'next/image'
+
 /* the best is to put globalStyles outside the function App because if it's inside, it will be rendered every time the function App is rendered */
 globalStyles()
 
@@ -11,7 +13,7 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <Container>
       <Header>
-        <img src={logoImg.src} alt="" />
+        <Image src={logoImg.src} alt="" width={130} height={52} />
       </Header>
 
       <Component {...pageProps} />
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,25 +1,60 @@
-import { styled } from '../styles'
+import Image from 'next/image'
 
-const Button = styled('button', {
-  backgroundColor: '$green300',
-  borderRadius: 4,
-  border: 0,
-  padding: '4px 8px',
+import { useKeenSlider } from 'keen-slider/react'
 
-  span: {
-    fontWeight: 'bold',
-  },
+import { HomeContainer, Product } from '../styles/pages/home'
 
-  '&:hover': {
-    filter: 'brightness(0.8)',
-  },
-})
+import shirt1 from '../assets/shirts/1.png'
+import shirt2 from '../assets/shirts/2.png'
+import shirt3 from '../assets/shirts/3.png'
+import shirt4 from '../assets/shirts/4.png'
+
+import 'keen-slider/keen-slider.min.css'
 
 export default function Home() {
+  const [sliderRef] = useKeenSlider({
+    slides: {
+      perView: 3,
+      spacing: 48,
+    },
+  })
+
   return (
-    <Button>
-      <span>Teste</span>
-      Enviar
-    </Button>
+    <HomeContainer ref={sliderRef} className="keen-slider">
+      <Product className="keen-slider__slide">
+        <Image src={shirt1} width={520} height={480} alt="" />
+
+        <footer>
+          <strong>Shirt X</strong>
+          <span>$ 79.90</span>
+        </footer>
+      </Product>
+      <Product className="keen-slider__slide">
+        <Image src={shirt2} width={520} height={480} alt="" />
+
+        <footer>
+          <strong>Shirt Y</strong>
+          <span>$ 79.90</span>
+        </footer>
+      </Product>
+
+      <Product className="keen-slider__slide">
+        <Image src={shirt3} width={520} height={480} alt="" />
+
+        <footer>
+          <strong>Shirt Z</strong>
+          <span>$ 79.90</span>
+        </footer>
+      </Product>
+
+      <Product className="keen-slider__slide">
+        <Image src={shirt4} width={520} height={480} alt="" />
+
+        <footer>
+          <strong>Shirt A</strong>
+          <span>$ 79.90</span>
+        </footer>
+      </Product>
+    </HomeContainer>
   )
 }
