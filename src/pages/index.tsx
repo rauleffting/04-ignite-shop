e47c9dec34fc284a40b1bf54164533ce813a82c1
raul <rauleffting94@gmail.com>
2023-05-04 12:41:30 -0300

    add products from stripe

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,8 +10,21 @@ import shirt3 from '../assets/shirts/3.png'
 import shirt4 from '../assets/shirts/4.png'
 
 import 'keen-slider/keen-slider.min.css'
+import { useEffect, useState } from 'react'
+import { stripe } from '../lib/stripe'
+import { GetServerSideProps } from 'next'
+import Stripe from 'stripe'
 
-export default function Home() {
+interface HomeProps {
+  products: {
+    id: string
+    name: string
+    imageUrl: string
+    price: number
+  }[]
+}
+
+export default function Home({ products }: HomeProps) {
   const [sliderRef] = useKeenSlider({
     slides: {
       perView: 3,
@@ -21,40 +34,54 @@ export default function Home() {
 
   return (
     <HomeContainer ref={sliderRef} className="keen-slider">
-      <Product className="keen-slider__slide">
-        <Image src={shirt1} width={520} height={480} alt="" />
+      {products.map((product) => {
+        return (
+          <Product key={product.id} className="keen-slider__slide">
+            <Image src={product.imageUrl} width={520} height={480} alt="" />
 
-        <footer>
-          <strong>Shirt X</strong>
-          <span>$ 79.90</span>
-        </footer>
-      </Product>
-      <Product className="keen-slider__slide">
-        <Image src={shirt2} width={520} height={480} alt="" />
-
-        <footer>
-          <strong>Shirt Y</strong>
-          <span>$ 79.90</span>
-        </footer>
-      </Product>
-
-      <Product className="keen-slider__slide">
-        <Image src={shirt3} width={520} height={480} alt="" />
-
-        <footer>
-          <strong>Shirt Z</strong>
-          <span>$ 79.90</span>
-        </footer>
-      </Product>
-
-      <Product className="keen-slider__slide">
-        <Image src={shirt4} width={520} height={480} alt="" />
-
-        <footer>
-          <strong>Shirt A</strong>
-          <span>$ 79.90</span>
-        </footer>
-      </Product>
+            <footer>
+              <strong>{product.name}</strong>
+              <span>$ {product.price.toFixed(2)}</span>
+            </footer>
+          </Product>
+        )
+      })}
     </HomeContainer>
   )
 }
+
+/* 
+With the getServerSideProps the page will be displayed only after all data was fetched and it's available 
+*/
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  // await new Promise((resolve) => setTimeout(resolve, 8000))
+  // console.log('rodou')
+
+  const response = await stripe.products.list({
+    expand: ['data.default_price'],
+  })
+
+  const products = response.data.map((product) => {
+    const price = product.default_price as Stripe.Price
+
+    return {
+      id: product.id,
+      name: product.name,
+      imageUrl: product.images[0],
+      url: product.url,
+      price: price.unit_amount / 100,
+    }
+  })
+
+  return {
+    props: {
+      products,
+    },
+  }
+}
+
+/*
+Also, we can put sensitive code inside the getServerSideProps, 
+because the user will not have access to data inside it
+*/
